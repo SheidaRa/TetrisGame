34f3e973391ae7361fa4aaac7c908e0ab7bc3fc4
Lily Ewing <77802704+LilyEwing@users.noreply.github.com>
2022-03-30 14:20:00 -0500

    added more shapes for pieces

--- TetrisGame/Piece.swift
+++ TetrisGame/Piece.swift
@@ -16,14 +16,43 @@ class Piece : SKNode{
     override init() {
         super.init()
         
-        let ltetris = [
+        let box = [
             (0,0),
             (0,1),
             (1,0),
             (1,1)
         ]
         
-        let piece = AssembleBlockChoice(structurePos: ltetris, fillColor: SKColor.gray, lineColor: SKColor.red, lineWidth: pieceLineW)
+        let zShape = [
+            (1,0), (2,0),
+            (1,1),
+            (1,2), (0,2)]
+        
+        let cornerShape = [
+            (1,0),
+            (1,1),
+            (1,2), (2,2), (3,2)]
+        
+        let pShape = [
+            (0,0),
+            (0,1),(1,1),
+            (0,2),(1,2)]
+        
+        let uShape = [
+            (1,1),(2,1), (3,1),
+            (1,2),(3,2)]
+        
+        let tShape = [
+            (1,0),
+            (1,1),
+            (0,2), (1,2),(2,2)]
+        
+        let stairShape = [
+            (3,1),
+            (3,2), (2,2),
+            (2,3), (1,3)]
+        
+        let piece = AssembleBlockChoice(structurePos: stairShape, fillColor: SKColor.gray, lineColor: SKColor.red, lineWidth: pieceLineW)
         addChild(piece)
     }
 }
