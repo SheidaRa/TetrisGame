b3e820bcaa14cf5c6e6e65036d62c11055954bf8
Sheida Rashidi <84582226+SheidaRa@users.noreply.github.com>
2022-04-28 13:53:42 -0500

    Created main menu screen images

--- TetrisGame/Main/MainMenu.swift
+++ TetrisGame/Main/MainMenu.swift
@@ -9,19 +9,35 @@ import SpriteKit
 
 
 class MainMenu: SKScene {
-    override func didMove (to view:SKView){
-        print("this is mainMenu")
-        
-    }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
-            if touch == touches.first{
-                print("going to Gameplay")
-                ACTManager.shared.transition(fromScene: self , toScene: .MainMenu, transition: SKTransition.moveIn(with: .right, duration: 0.5))
-            
-            }
-        }
+    var buttonImage = SKSpriteNode()
+
+    override func didMove(to view:SKView){
+        let title = SKSpriteNode(
+            texture: SKTexture(
+                image: UIImage(named: "Title")!))
+        title.position = CGPoint(x: size.width * 0.5, y: size.height * 0.8)
+        title.setScale(0.3)
+        addChild(title)
+        
+
+        let logo = SKSpriteNode(
+            texture: SKTexture(
+                image: UIImage(named: "MainImg")!))
+        logo.position = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
+        logo.setScale(0.3)
+        addChild(logo)
     }
-}
+        
+    
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        for touch in touches {
+//            if touch == touches.first{
+//                print("going to Gameplay")
+//
+//            }
+//        }
+    }
+
+
  
