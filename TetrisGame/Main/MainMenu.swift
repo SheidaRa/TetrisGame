a8e1654518a5fac6386bae2181b341eec80060d1
Sheida Rashidi <84582226+SheidaRa@users.noreply.github.com>
2022-04-28 23:04:12 -0500

    Update MainMenu.swift

--- TetrisGame/Main/MainMenu.swift
+++ TetrisGame/Main/MainMenu.swift
@@ -10,14 +10,21 @@ import SpriteKit
 
 class MainMenu: SKScene {
     
+
     var buttonImage = SKSpriteNode()
+    
 
     override func didMove(to view:SKView){
+        
+//        let background = SKSpriteNode(imageNamed: "TetrisBackground")
+//        self.addChild(background)
+//
+        
         let title = SKSpriteNode(
             texture: SKTexture(
                 image: UIImage(named: "Title")!))
         title.position = CGPoint(x: size.width * 0.5, y: size.height * 0.8)
-        title.setScale(0.3)
+        title.setScale(0.4)
         addChild(title)
         
 
@@ -27,9 +34,23 @@ class MainMenu: SKScene {
         logo.position = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
         logo.setScale(0.3)
         addChild(logo)
+        
+        
+        let buttonImage = SKSpriteNode(
+            texture: SKTexture(
+                image: UIImage(named: "StartB")!))
+        buttonImage.position = CGPoint(x: size.width * 0.5, y: size.height * 0.15)
+        buttonImage.setScale(0.4)
+        addChild(buttonImage)
+        
+        
+        
+        
+        
+        
     }
         
-    
+ 
 //    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 //        for touch in touches {
 //            if touch == touches.first{
